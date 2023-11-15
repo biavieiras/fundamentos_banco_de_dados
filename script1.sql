2631bc73c7bd91ee8be39898f5777c9deef6c3a9
gcirineu18 <guilhermecirineu@hotmail.com>
2023-11-14 23:41:35 -0300

    criação parcial das tabelas

--- script1.sql
+++ script1.sql
@@ -67,8 +67,23 @@ on
 -- bia
 create table playlist
 (
-
+cod_playlist int not null,
+nome_playlist nvarchar(50) not null,
+dt_criacao date not null,
+----provavelmente errado-------
+tempo_exec dec(10,10),
+----------------------------
+constraint PK_playlist primary key (cod_playlist)
 ) on spotper_fg01
+
+--bia
+create table periodo_musical
+(
+cod_pm smallint not null,
+descricao_pm nvarchar(100) not null,
+intervalo varchar(20),
+constraint PK_cod_pm primary key (cod_pm)
+) on spotper_fg02
 
 --bruna
 create table faixa
@@ -76,10 +91,16 @@ create table faixa
 
 ) on spotper_fg01
 
+
 --bia
 create table faixa_playlist 
 (
-
+id_playlist int not null,
+cod_faixa not null,
+constraint FK_cod_playlist foreign key(id_playlist) references playlist(cod_playlist)
+on delete no action on update cascade
+constraint FK_cod_faixa foreign key(cod_faixa) references playlist(num_faixa)
+on delete no action on update cascade
 ) on spotper_fg01
 
 
@@ -87,17 +108,89 @@ create table faixa_playlist
 create table composicao 
 (
 cod_composicao smallint not null,
-descricao_compos  
+descricao_compos nvarchar(200) not null,
+tipo_composicao varchar(10) not null
+
+constraint PK_composicao primary key (cod_composicao)
+constraint CK_tipo_composicao check (tipo_composicao in ('sinfonia', 'opera', 'sonata', 'concerto'))
+
 ) on spotper_fg02
 
 
+--bruna
+create table interprete
+(
+cod_interprete smallint not null,
+nome_interprete nvarchar(50) not null,
+tipo_interprete nvarchar(10) not null
+
+constraint PK_cod_interprete check(cod_interprete in ('orquestra', 'trio', 'quarteto', 'ensemble', 'soprano', 'tenor'))
+
+) on spotper_fg02
+
+
+--bruna
+create table faixa_interprete
+(
+cod_interprete smallint not null,
+num_faixa smallint not null
+
+constraint FK_cod_interprete foreign key (cod_interprete) 
+references interprete (cod_interprete)
+
+constraint FK_num_faixa foreign key (num_faixa)
+references faixa (num_faixa)
+
+)
 
 
 --gui
 create table gravadora
 (
-cod_gravad smallint not null
+cod_gravad smallint not null,
+nome nvarchar(100) not null,
+cep varchar(11),
+cidade nvarchar(50),
+rua nvarchar(100),
+end_site varchar(200)
 
+CONSTRAINT gravadora_PK primary key (cod_gravad)
+
+) on spotper_fg02
+
+
+create table telefone 
+(
+    fone varchar(10) not null,
+    tipo_fone varchar(50) not null 
+    cod_gravadora smallint not null,
+    
+    CONSTRAINT fone_PK PRIMARY KEY (fone)
+
+    CONSTRAINT telefone_CK CHECK (tipo_telefone IN ('celular', 'fixo'))
+    
+    CONSTRAINT telefone_FK_gravadora FOREIGN KEY (cod_gravadora)
+    REFERENCES gravadora (cod_gravad)  ON UPDATE cascade ON DELETE CASCADE
+
+    
+) on spotper_fg02
+
+
+create table album
+(
+    cod_album smallint not null,
+    descricao_album nvarchar(150) not null,
+    nome nvarchar(50) not null,
+    tipo_compra nvarchar(50) not null,
+    pr_compra decimal(7,2) not null,
+    dt_compra date not null,
+    dt_gravacao date not null,
+    
+
+    CONSTRAINT cod_album_PK PRIMARY KEY (cod_album)
 
 
 ) on spotper_fg02
+
+
+
