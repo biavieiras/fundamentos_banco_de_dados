5acd13d6f26feea11dd425f33ea667c4b3ed668e
gcirineu18 <guilhermecirineu@hotmail.com>
2023-11-15 23:02:58 -0300

    inclusão da visão

--- script1.sql
+++ script1.sql
@@ -96,7 +96,8 @@ create table faixa
 create table faixa_playlist 
 (
 id_playlist int not null,
-cod_faixa not null,
+cod_faixa int not null,
+
 constraint FK_cod_playlist foreign key(id_playlist) references playlist(cod_playlist)
 on delete no action on update cascade
 constraint FK_cod_faixa foreign key(cod_faixa) references playlist(num_faixa)
@@ -201,6 +202,24 @@ create table album
 	CONSTRAINT data_gravacao_CK CHECK  (dt_gravacao> '2000-01-01')
 
 ) 
+
+
+
+-- questão 5
+-- Criar uma visão materializada que tem como atributos o nome da playlist e a
+-- quantidade de álbuns que a compõem.
+
+
+
+create view playlist_qtde_albuns(nome_playlist, qtde_albuns)
+with schemabinding
+as
+   select nome_playlist, count_big(cod_album) from dbo.playlist, dbo.faixa_playlist,
+   dbo.faixa f, dbo.meio_faixa mfa , dbo.meio_fisico mfi, dbo.album a
+   
+   where cod_playlist = id_playlist and cod_faixa = f.num_faixa and f.num_faixa = mfa.num_faixa
+   and mfa.id_meio_fisico = mfi.id_meio_fisico and a.cod_album = mfi.cod_album
+   group by nome_playlist
 
 
 
